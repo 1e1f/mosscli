a1c17206a05b2b2dd07656b2ace5c8921a373c36
Leif Shackelford <structuresound@gmail.com>
2018-07-04 12:55:03 -0700

    env files

--- test/cli.test.ts
+++ test/cli.test.ts
@@ -1,17 +1,16 @@
 import { readFileSync, unlinkSync } from 'fs';
-import * as yaml from 'js-yaml';
-import { join } from 'path';
 import { assert } from 'chai';
 
 import { getFunctions } from 'js-moss';
-import { run } from '../src/cli';
+import { moss } from '../src';
 
 const expect: any = {
   'production.yaml': `libraryTarget: es5\n`,
   'debug.yaml': `libraryTarget: es6\n`,
   'debug.json': `{
   "libraryTarget": "es6"
-}`
+}`,
+  '.env.debug': `LIBRARY_TARGET=es6\n`
 }
 
 describe('moss', () => {
@@ -20,7 +19,7 @@ describe('moss', () => {
   });
   it('can write files using <write function', () => {
     process.chdir(__dirname);
-
+    
     Object.keys(expect).forEach(key => {
       try {
         unlinkSync(key);
@@ -28,11 +27,7 @@ describe('moss', () => {
       catch (e) { }
     });
 
-    const args = {
-      _: ['test.yaml']
-    }
-
-    run(args);
+    moss('test.moss');
 
     Object.keys(expect).forEach(key => {
       let file;
